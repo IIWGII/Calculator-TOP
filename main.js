912bc589e142d83524b7df036aa37b91d9e1ea26
IIWGII <Himdi51@gmail.com>
2023-01-22 19:57:58 +0100

    fixed bugg

--- main.js
+++ main.js
@@ -59,7 +59,7 @@ class Calculator {
     }
 
     getDisplayNumber(number) {
-        const floatNumber = parseFloat(number)
+        const stringNumber = number.toString()
         const integerDigits = parseFloat(stringNumber.split('.')[0])
         const decimalDigits = stringNumber.split('.')[1]
         let integerDisplay 
@@ -67,8 +67,7 @@ class Calculator {
             integerDisplay = ''
         } else {
             integerDisplay = integerDigits.toLocaleString('en', {
-                maximumFractionDigits: 0
-            })
+                maximumFractionDigits: 0 })
         }
         if (decimalDigits != null) {
             return `${integerDisplay}.${decimalDigits}`
