b2cd7b63c5963e04176db33a73c4fde0842f78a2
IIWGII <Himdi51@gmail.com>
2023-01-22 18:54:54 +0100

    making compute logic

--- main.js
+++ main.js
@@ -1,20 +1,122 @@
-const numbers = document.querySelectorAll('.nbr');
-const result = document.querySelector('.result');
-const equals = document.getElementById('equals');
-const multiply = document.getElementById('multiplication');
-const subtraction = document.getElementById('subtraction');
-const division = document.getElementById('division');
-const add = document.getElementById('addition');
-const clear = document.getElementById('clear');
-const currentOperations = document.getElementById('current-operations');
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+    }
 
+    clear() {
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
 
-numbers.forEach((number) => {
-    number.addEventListener('click', (e) => {
-        if (!e.target.classList.contains('operation')) {
-            
-            currentOperations.innerText += e.target.innerText;
+    }
+
+    delete() {
+
+    }
+
+    appendNumber(number) {
+        if (number === '.' && this.currentOperand.includes('.')) return
+        this.currentOperand = this.currentOperand.toString() + number.toString()
+    }
+
+    chooseOperation(operation) {
+        if (this.currentOperand === '') return
+        if (this.previousOperand !== '') {
+            this.compute()
         }
-        });
-});
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
+ 
+    }
+
+    compute() {
+        let computation
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if (isNaN(prev) || isNaN(current)) return
+        switch (this.operation) {
+            case '+':
+                computation = prev + current
+                break
+            case '-':
+                computation = prev - current
+                break
+            case '*':
+                computation = prev * current
+                break
+            case '/':
+                computation = prev / current
+                break
+            default:
+                return
+            
+        }
+
+    }
+
+    updateDisplay() {
+      this.currentOperandTextElement.innerText = this.currentOperand
+      this.previousOperandTextElement.innerText = this.previousOperand
+    }
+
+}
+
+
+
+
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+
+
+
+
+
+// const numbers = document.querySelectorAll('.nbr');
+// const result = document.querySelector('.result');
+// const equals = document.getElementById('equals');
+// const multiply = document.getElementById('multiplication');
+// const subtraction = document.getElementById('subtraction');
+// const division = document.getElementById('division');
+// const add = document.getElementById('addition');
+// const clear = document.getElementById('clear');
+// const currentOperations = document.getElementById('current-operations');
+
+
+// numbers.forEach((number) => {
+//     number.addEventListener('click', (e) => {
+//         if (!e.target.classList.contains('operation')) {
+            
+//             currentOperations.innerText += e.target.innerText;
+//         }
+//         });
+// });
 
